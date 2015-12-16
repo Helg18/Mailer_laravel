e14b950653bd2a0597bb127eefcd63724cd31178
Henry Leon Gomez <Helg18@gmail.com>
2015-12-15 21:55:45 -0430

    Commit Inicial

--- resources/views/admin/template/partials/footer.blade.php
+++ resources/views/admin/template/partials/footer.blade.php
@@ -2,6 +2,6 @@
   <div class="panel-footer" align='center'> 
   Sitio web desarrollado por: <br> 
   <a href="mailto:helg18@gmail.com" target="_blank">Ing. Henry Leon</a><br>
-  <div align="left" class="col-xs-9">Mailer™&nbsp;&nbsp;&nbsp;© CopyRigth ® Reservados todos los derechos</div><div align="right" class="col-xs-3">Otro Producto <a href="almfullstore.com" target="_blank">ALM Full Store, C.A</a></div><br>
+  <div align="left" class="col-xs-9">Mailer™&nbsp;&nbsp;&nbsp;© CopyRigth ® Reservados todos los derechos</div><div align="right" class="col-xs-3">Otro Producto <a href="http://almfullstore.com" target="_blank">ALM Full Store, C.A</a></div><br>
   </div>
 </div>
